1e8a8ff9abd58d9613c2f88e3b09c43233f768e2
bamchoh <bamchoh@gmail.com>
2021-02-14 04:07:19 +0900

    Fix bbs_dir

--- utils.php
+++ utils.php
@@ -1,5 +1,6 @@
 <?php
 function redirect_to($uri) {
+    global $bbs_dir;
     $baseurl = (empty($_SERVER['HTTPS']) ? 'http://' : 'https://') . $_SERVER['HTTP_HOST'];
     $url = $baseurl . $bbs_dir . $uri;
     header('Location: ' . $url);
